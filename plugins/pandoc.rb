8acbc89503b3abb47466ef117feb7b87a13b05a8
huydo <doxuanhuy@gmail.com>
2013-11-08 13:51:46 +0900

    Octopress push new post

--- plugins/pandoc.rb
+++ plugins/pandoc.rb
@@ -1,16 +1,17 @@
 require 'open3'
 module Jekyll
+
 # Just return html5
 class MarkdownConverter
-def convert(content)
+  def convert(content)
     flags  = @config['pandoc']['flags']
     output = ''
     Open3::popen3("pandoc -t html5 #{flags}") do |stdin, stdout, stderr|
-        stdin.puts content
-        stdin.close
-        output = stdout.read.strip
+      stdin.puts content
+      stdin.close
+      output = stdout.read.strip
     end
     output
     end
-end
+  end
 end
